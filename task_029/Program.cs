2b343079bbc3bcb2edf3ab01820c56df9b52beed
Haman692 <Haman692@yandex.ru>
2022-12-04 22:59:21 +0700

    task029

---
+++ task_029/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int DataNumber(string meseg)
+{
+    Console.Write(meseg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void PrintArray(int[] charr)
+{
+    int i;
+    for (i = 0; i < charr.Length - 1; i++)
+    {
+        Console.Write(charr[i] + ", ");
+    }
+    Console.Write(charr[i]);
+}
+
+void RandomIndex(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1, 99);
+    }
+}
+
+Console.Clear();
+int index = DataNumber("Укажите раззмер масива ");
+int[] array = new int[index];
+RandomIndex(array);
+PrintArray(array);
