fef44d4656cdec47097eed9960dfdc96e8fdcf18
Haman692 <Haman692@yandex.ru>
2022-12-04 22:09:31 +0700

    task025.1

---
+++ task_025.1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.(С помощю цикла)
+
+
+int DataNumber(string meseg)
+{
+    Console.Write(meseg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int numberA = DataNumber("Введите число А ");
+int numberB = DataNumber("Введите число В ");
+
+double exp = numberA;
+for (int i = 1; i < numberB; i++)
+{
+    exp = exp*numberA;
+}
+Console.WriteLine($"Степень числа {numberA} в {numberB} = {exp}");
