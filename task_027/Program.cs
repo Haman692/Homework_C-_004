83ad12839f14258d428d06c3896bc4ef91799785
Haman692 <Haman692@yandex.ru>
2022-12-04 22:41:02 +0700

    task027

---
+++ task_027/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int DataNumber(string meseg)
+{
+    Console.Write(meseg);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int Decay(int number)
+{
+    int sum;
+    for (sum = 0; number != 0; number = number/10)
+    {
+        int count = number % 10;
+        sum = sum + count;
+        // Console.WriteLine(sum); //Поверка(что происходит в цикле)
+    }
+    return sum;
+}
+
+int num = DataNumber("Введите число ");
+int sum = Decay(num);
+Console.WriteLine(sum);
